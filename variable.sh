51a88740b35025e4f01edbd8174b76b59b9f5f01
sagarmagar <sagarmagar0709@gmail.com>
2024-06-26 00:29:30 +0530

    bash auto push

--- variable.sh
+++ variable.sh
@@ -6,3 +6,6 @@ b=def
 echo $a
 echo "value of the variable a is $a"
 echo "value of the variable b is ${a}"
+
+# variable are declared as a and b
+# you can fetch data in condition by using variable like line no 8

96a6fe588104f83251eae42c5ab2d1939d23b9d1
sagarmagar <sagarmagar0709@gmail.com>
2024-06-25 23:54:47 +0530

    bash auto push

--- charector.sh
+++ charector.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo line1
-echo -e "line2\nline3"
+echo -e "line2\nline3" 
 echo -e "line5\tline6"
 echo -e "line7\n\tline8"
+
+# echo -e ----n used for new linw
+# echo -e -----t used for tab that is 3 space
+# n\t  -----------used as new line + tab

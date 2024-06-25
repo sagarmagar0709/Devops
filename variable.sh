ef3ff263d4c31a6842e692e68f29940943494f11
sagarmagar <sagarmagar0709@gmail.com>
2024-06-26 00:38:57 +0530

    bash auto push

--- variable.sh
+++ variable.sh
@@ -9,3 +9,11 @@ echo "value of the variable b is ${a}"
 
 # variable are declared as a and b
 # you can fetch data in condition by using variable like line no 8
+
+
+custdata = "shipping"
+
+rm-rf /data/prod/$(custdata)=====> /data/prod/shipping
+# here you can gve variable custdata is shiiping , hence if you delete custdata it will delete the shipping.
+# if you give values as another like insted of custdata if you give appdata it will delete directory here
+# here it concider as rm-rf /data/prod and delete prod

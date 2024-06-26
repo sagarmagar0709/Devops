d6b2eaab7a43ac4899273e183ffb18750cfc4b89
sagarmagar <sagarmagar0709@gmail.com>
2024-06-26 15:25:56 +0530

    bash auto push

--- special variable.sh
+++ special variable.sh
@@ -2,7 +2,9 @@ a =3000
 echo "value of a is $a"
 echo "script name is : $0"
 Team =$1
+project =$2
 
 # here we can print the script name as output
 
 echo -e "name of the running script is : $TEAM"
+echo -e "name of the project is :$project"
